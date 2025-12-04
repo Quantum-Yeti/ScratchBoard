cd24a232135ede5ebf0c9265b8bbe7640e75e951
James Keenan <monsterbrains@protonmail.com>
2025-12-04 14:34:37 -0500

    style(contacts_editor_view.py): refine style of edit and add contact dialog windows with tooltips, window size, and images

--- views/contacts_editor_view.py
+++ views/contacts_editor_view.py
@@ -1,4 +1,8 @@
+from PySide6.QtCore import QSize
+from PySide6.QtGui import QIcon, QPixmap, Qt
 from PySide6.QtWidgets import QDialog, QVBoxLayout, QLineEdit, QPushButton, QLabel
+
+from utils.resource_path import resource_path
 
 class ContactEditorPanel(QDialog):
     """
@@ -28,44 +32,74 @@ class ContactEditorPanel(QDialog):
         # Set window title depending on editing or creating a contact.
         if contact_id:
             self.setWindowTitle("Edit Contact")
+            self.setMinimumSize(325, 525)
         else:
             self.setWindowTitle("New Contact")
+            self.setMinimumSize(325, 525)
 
         # Main vertical layout
         layout = QVBoxLayout(self)
 
+        # Top image
+        self.image = QLabel()
+        self.image.setPixmap(QPixmap(resource_path("resources/icons/astronaut_wave.png")))
+        self.image.setAlignment(Qt.AlignCenter)
+        layout.addWidget(self.image)
+
         # Name input
         self.name_input = QLineEdit(name)
         self.name_input.setPlaceholderText("Name")
+        self.name_input.setToolTip("Enter a name")
         layout.addWidget(QLabel("Name:"))
         layout.addWidget(self.name_input)
 
         # Phone input
         self.phone_input = QLineEdit(phone)
         self.phone_input.setPlaceholderText("Phone")
+        self.phone_input.setToolTip("Enter a phone number")
         layout.addWidget(QLabel("Phone:"))
         layout.addWidget(self.phone_input)
 
         # Email input
         self.email_input = QLineEdit(email)
         self.email_input.setPlaceholderText("Email")
+        self.email_input.setToolTip("Enter an email address")
         layout.addWidget(QLabel("Email:"))
         layout.addWidget(self.email_input)
 
         # Website input
         self.website_input = QLineEdit(website)
         self.website_input.setPlaceholderText("Website")
+        self.website_input.setToolTip("Enter a website address")
         layout.addWidget(QLabel("Website:"))
         layout.addWidget(self.website_input)
 
         # Save button
         save_btn = QPushButton("Save")
+        save_btn.setToolTip("Save the contact")
+        save_btn.setIcon(QIcon(resource_path("resources/icons/save.png")))
+        save_btn.setIconSize(QSize(24, 24))
+        save_btn.setStyleSheet("""
+            QPushButton {
+                text-align: center;
+                padding: 4px;
+            }
+        """)
         save_btn.clicked.connect(self.save)
         layout.addWidget(save_btn)
 
         # Delete button called if editing
         if delete_callback:
             delete_btn = QPushButton("Delete")
+            delete_btn.setToolTip("Delete the contact")
+            delete_btn.setIcon(QIcon(resource_path("resources/icons/delete.png")))
+            delete_btn.setIconSize(QSize(24, 24))
+            delete_btn.setStyleSheet("""
+                QPushButton {
+                    text-align: center;
+                    padding: 4px;
+                }
+            """)
             delete_btn.clicked.connect(self.delete)
             layout.addWidget(delete_btn)
 
