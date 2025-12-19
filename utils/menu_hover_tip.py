9d1ef1ffc01b9822ca86b79bd92c3b1cd3b752bc
James Keenan <monsterbrains@protonmail.com>
2025-12-19 02:53:46 -0500

    docs(menu_hover_tip.py): add docstring for readability

--- utils/menu_hover_tip.py
+++ utils/menu_hover_tip.py
@@ -3,8 +3,17 @@ from PySide6.QtWidgets import QToolTip, QMenu
 
 
 class HoverToolTip(QObject):
+    """
+    Event filter that displays tooltips for QMenu actions when hovering with a mouse.
+    """
     def eventFilter(self, obj, event):
-        if isinstance(obj, QMenu) and event.type() == QEvent.ToolTip:
+        """
+        Intercepts events for QMenu actions when hovering with a mouse.
+        :param obj: QMenu object that receives the event.
+        :param event: The event object to filter.
+        :return: boolean: True if the event was handled (display/hide), False otherwise.
+        """
+        if isinstance(obj, QMenu) and event.type() == QEvent.Type.ToolTip:
             action = obj.actionAt(event.pos())
             if action and action.toolTip():
                 QToolTip.showText(event.globalPos(), action.toolTip())
