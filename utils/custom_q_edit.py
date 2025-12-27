55aa52752894aa53273facdf6b3dd86c2089bdaf
James Keenan <monsterbrains@protonmail.com>
2025-12-27 13:43:22 -0500

    fix: insertFromMimeData to keep pasted text color white in CustomQEdit

--- utils/custom_q_edit.py
+++ utils/custom_q_edit.py
@@ -1,8 +1,15 @@
+from PySide6.QtGui import QColor
 from PySide6.QtWidgets import QTextEdit, QLineEdit
 from pygments.lexers import q
 
 
 class CustomQEdit(QTextEdit):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        # Force white text
+        self.setTextColor(QColor.fromRgb(255, 255, 255))
+
     def insertFromMimeData(self, source):
         if source.hasText():
             self.insertPlainText(source.text())
