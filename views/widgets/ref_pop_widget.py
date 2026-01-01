54b6d940de99847664bb6f254beba601aff769bb
James Keenan <monsterbrains@protonmail.com>
2026-01-01 18:52:06 -0500

    ref: override context menu for ref_pop_widget.py

--- views/widgets/ref_pop_widget.py
+++ views/widgets/ref_pop_widget.py
@@ -1,6 +1,7 @@
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QDialog, QVBoxLayout
 
+from utils.custom_context_menu import ContextMenuUtility
 from views.widgets.reference_widget import ReferenceWidget
 
 
@@ -10,18 +11,18 @@ class ReferencePopup(QDialog):
         self.setWindowTitle("Scratch Board: Custom Links")
         self.resize(500, 700)
 
-
-
-        # Allow minimize / close independently
+        # Allow minimizing / close independently
         self.setWindowFlags(
             self.windowFlags() |
-            Qt.WindowMinimizeButtonHint |
-            Qt.WindowCloseButtonHint
+            Qt.WindowType.WindowMinimizeButtonHint |
+            Qt.WindowType.WindowCloseButtonHint
         )
-        self.setWindowModality(Qt.NonModal)  # Non-blocking
+        self.setWindowModality(Qt.WindowModality.NonModal)  # Non-blocking
 
         layout = QVBoxLayout(self)
 
         # Embed your existing widget inside the dialog
         self.ref_view = ReferenceWidget(model)
         layout.addWidget(self.ref_view)
+
+        self.custom_context_menu = ContextMenuUtility(self.ref_view)
